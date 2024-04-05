9c62ca616f4239cc3d02c86df3909278b97abf6f
cobyg <cobyg@COBYS_LAPTOP>
2024-04-04 19:42:26 -0700

    Update

--- Prog120_lecture2/Program.cs
+++ Prog120_lecture2/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +13,42 @@ namespace Prog120_lecture2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");        }
+            Console.Write("Enter your name:");
+            string userName = Console.ReadLine();
+            Console.Write("display user name:" + userName);
+            Console.Write("What is the the date?");
+            string userDate = Console.ReadLine();
+            Console.Write($"The date is {userDate}");
+            Console.Write("What is the class?");
+
+
+            ;// Curly Brace
+            // Error
+            // Compile Time Error}
+        } // End of Main
+
+        //public static void Name() {}
+        public static void Practice()
+        {
+            string teachersName = "Will";
+
+            String firstName = "Will";
+            String lastName = "Cram";
+            Console.WriteLine(firstName + " " + lastName);
+            string fullName = firstName + " " + lastName;
+
+            Console.Write("Hello, ");
+            Console.WriteLine(teachersName);
+
+
+            {
+               
+            }
+
+        //Public static void StringExample()
+         }
+        // end of classs
     }
-}
+}// end of namespace
+
+    
